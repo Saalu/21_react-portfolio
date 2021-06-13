d82f89463ee0503c414d0c9d815f564ac5f8def3
Saalu <saaluissak@gmail.com>
2021-06-13 20:36:58 +0000

    Setup and folder structure

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./topbar.scss";
+function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper"></div>
+    </div>
+  );
+}
+
+export default Topbar;
