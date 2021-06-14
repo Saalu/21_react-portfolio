196f24851d8d757fc8fc578184c9713b6c8bea90
Saalu <saaluissak@gmail.com>
2021-06-14 11:01:43 +0000

    Intro section features

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./intro.scss";
+import { init } from "ityped";
 
 function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      showCursor: true,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Saalu Issaka</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
